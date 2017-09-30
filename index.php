04a5c3d0b9c6bcc355495957ee19485e1743f2b4
baw7 <baw7@njit.edu>
2017-09-30 11:25:59 -0400

    comparison

--- index.php
+++ index.php
@@ -6,6 +6,16 @@ print "The value of \$date: ".$date."<br>";
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
+
+if ($date === $tar > "0") {
+    print  "the future";
+    }
+elseif ($date === $tar < "0") {
+    print "the past";
+    }
+else ($date === $tar == "0") {
+    print "Oops";
+    }
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
