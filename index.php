cf13ec0576984cda6133688d2b23d4ea87b3a1c9
baw7 <baw7@njit.edu>
2017-09-30 11:15:00 -0400

    print

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $date =  date('Y-m-d', time());
 $date = str_replace('-', '/', $date);
-echo "The value of \$date: ".$date."<br>";;
+print "The value of \$date: ".$date."<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
